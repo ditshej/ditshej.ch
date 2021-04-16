dd915b6dbbf9ea4b4879000d730b214595f3af3f
Raphi W <ditshej@gmail.com>
2021-04-16 11:21:41 +0200

    initial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>👻</title>
+</head>
+<body style="width: 100vw; height: 100vh; overflow: hidden;">
+<?php
+$loops = 50000;
+$loop = 1;
+while ($loop < $loops) :
+$loop++;
+?>
+    👻
+<?php endwhile; ?>
+</body>
+</html>
